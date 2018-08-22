382abdaa01f3c01eb7b02fa1ffec89895ed2b692
Denel Manilov <denel.manilov@gmail.com>
2018-08-22 15:13:24 +0400

    add win support

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -1,10 +1,24 @@
-#include <unistd.h>
+#ifdef _WIN32
+    #include <windows.h>
+
+    void sleepWrap(unsigned milliseconds)
+    {
+        Sleep(milliseconds);
+    }
+#else
+    #include <unistd.h>
+
+    void sleepWrap(unsigned milliseconds)
+    {
+        sleep(milliseconds);
+    }
+#endif
 
 int main(void)
 {
 	while (true) 
 	{
-		sleep(1000);
+		sleepWrap(1000);
 	}
 	return 0;
 }
